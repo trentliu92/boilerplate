1d85448b1c5ca0c8c8ef4f183e515b7fe96d9133
Trent Liu <trentliu92@gmail.com>
2018-12-03 14:03:47 -0800

    init structure

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -8,13 +8,8 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Switch, Route } from 'react-router-dom';
+// import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
 import './style.scss';
 
 const App = () => (
@@ -25,13 +20,7 @@ const App = () => (
     >
       <meta name="description" content="A React.js Boilerplate application" />
     </Helmet>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/features" component={FeaturePage} />
-      <Route path="" component={NotFoundPage} />
-    </Switch>
-    <Footer />
+
   </div>
 );
 
