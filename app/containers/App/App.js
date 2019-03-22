5822447554323f6bda3054ae8061296c5dcd529c
Trent Liu <trentliu92@gmail.com>
2019-03-22 00:57:48 -0700

    init boilerplate

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -1,24 +1,9 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import Page from 'components/layout/Page';
-import HomePage from 'components/HomePage/HomePage';
 import './style.scss';
 
 const App = () => (
-  <div className="megasplash-container">
-    <div className="megasplash-home">
-      <Page
-        component={<HomePage />}
-      />
-    </div>
+  <div className="app">
+    Trent Liu Boilerplate
   </div>
 );
 
